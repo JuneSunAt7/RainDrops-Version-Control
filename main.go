437bdcca1f3f78ce6b3c7402e13f8d96abaa732d
Gem13 <jprogram@bk.ru>
2025-01-06 01:43:00 +0300

    create session variables

--- main.go
+++ main.go
@@ -3,34 +3,42 @@ package main
 import (
     "rdvc/init_dir"
     "rdvc/networking"
-	"os"
+    "os"
     "strings"
 
-	"github.com/pterm/pterm"
+    "github.com/pterm/pterm"
+)
+
+var (
+    currentRepoName string
+    currentRepoPath string
 )
 
 func printHelp() {
     pterm.FgGreen.Println("rdvc - RainDrops Version Control")
     pterm.FgBlue.Println("Usage:")
     pterm.FgCyan.Println(" rdvc init -p <path_to_directory> -n <repo_name>    Initialize a controlled directory")
-    pterm.FgCyan.Printfln(" rdvc keep -m <message> -u <user_name> -n <repo_name>    Keep changes with a message")
-    pterm.FgCyan.Printfln(" rdvc line -n <repo_name> -o  <name_line>     Create line for current changes in repo")
-    pterm.FgCyan.Printfln(" rdvc checkout -n <repo_name> -o <line_name>      Go to line")
-    pterm.FgCyan.Printfln(" rdvc roll -n <repo_name>      Pullback to choosed version of file")
+    pterm.FgCyan.Printfln(" rdvc keep -m <message> -u <user_name>    Keep changes with a message")
+    pterm.FgCyan.Printfln(" rdvc line -o <name_line>      Create line for current changes in repo")
+    pterm.FgCyan.Printfln(" rdvc checkout -o <line_name>      Go to line")
+    pterm.FgCyan.Printfln(" rdvc roll      Pullback to choosed version of file")
+    pterm.FgCyan.Printfln(" ")
+    pterm.FgCyan.Printfln(" rdvc session -u <user_name> -n <repo_name>     Start work session and store data")
     pterm.FgCyan.Printfln(" ")
     pterm.FgCyan.Printfln(" rdvc set -u <username> -p <password>     Setup config for cloud storage")
-    pterm.FgCyan.Printfln(" rdvc send -n <repo_name>     Send keeped changes to cloud")
-    pterm.FgCyan.Printfln(" rdvc get -n <repo_name>     Get list changes from repo in cloud")
+    pterm.FgCyan.Printfln(" rdvc send     Send kept changes to cloud")
+    pterm.FgCyan.Printfln(" rdvc get     Get list changes from repo in cloud")
    
     pterm.FgCyan.Printfln(" ")
     pterm.FgMagenta.Printfln(" rdvc help     Display this help message")
 }
-func check_args(){
+
+func checkArgs() {
     if len(os.Args) < 2 {
         printHelp()
         return
     }
-    switch os.Args[1]{
+    switch os.Args[1] {
     case "help":
         printHelp()
         return
@@ -38,9 +46,21 @@ func check_args(){
         pathFlag := os.Args[3]
         init_dir.InitInvisible(pathFlag)
         init_dir.CreateSettings(pathFlag, os.Args[5])
+    case "session":
+        if len(os.Args) < 5 {
+            pterm.Error.Println("You must specify a user name and repo name for the session.")
+            return
+        }
+        currentRepoName = os.Args[5]
+        currentRepoPath = init_dir.ReadFromReg(currentRepoName)
+        // Вы здесь можете также запустить какой-либо код для начала сессии
+
     case "keep":
-        repoPath := init_dir.ReadFromReg(os.Args[7])
-        versionControl := init_dir.NewVCS(repoPath)
+        if currentRepoPath == "" {
+            pterm.Error.Println("You must start a session before using this command.")
+            return
+        }
+        versionControl := init_dir.NewVCS(currentRepoPath)
         message := os.Args[3]
         author := os.Args[5]
 
@@ -49,33 +69,51 @@ func check_args(){
             pterm.Error.Println("Error in keeping:", err)
         }
     case "line":
-        repoPath := init_dir.ReadFromReg(os.Args[3])
-        vcs := init_dir.NewVCS(repoPath)
-        err := vcs.CreateBranch(os.Args[5])
+        if currentRepoPath == "" {
+            pterm.Error.Println("You must start a session before using this command.")
+            return
+        }
+        vcs := init_dir.NewVCS(currentRepoPath)
+        err := vcs.CreateBranch(os.Args[3])
         if err != nil {
             pterm.Error.Println("Error creating line:", err)
             return
         }
     case "checkout":
-        repoPath := init_dir.ReadFromReg(os.Args[3])
-        vcs := init_dir.NewVCS(repoPath)
-        err := vcs.CheckoutBranch(os.Args[5])
+        if currentRepoPath == "" {
+            pterm.Error.Println("You must start a session before using this command.")
+            return
+        }
+        vcs := init_dir.NewVCS(currentRepoPath)
+        err := vcs.CheckoutBranch(os.Args[3])
         if err != nil {
-            pterm.Error.Println("Ошибка переключения на ветку:", err)
+            pterm.Error.Println("Error switching branch:", err)
             return
         }
     case "send":
-        networking.UploadKeeps(os.Args[3])
+        // Если repoName не требуется, используем текущий
+        if currentRepoName == "" {
+            pterm.Error.Println("You must start a session to send changes.")
+            return
+        }
+        networking.UploadKeeps(currentRepoName)
     case "get":
-        networking.GetKeeps(os.Args[3])
+        // Если repoName не требуется, используем текущий
+        if currentRepoName == "" {
+            pterm.Error.Println("You must start a session to get changes.")
+            return
+        }
+        networking.GetKeeps(currentRepoName)
     case "set":
         networking.Connect(os.Args[3], os.Args[5])
-
         reg_data := []string{os.Args[3], os.Args[5]}
-        init_dir.CreateSettings( strings.Join(reg_data, ","), "reg_data" )
+        init_dir.CreateSettings(strings.Join(reg_data, ","), "reg_data")
     case "roll":
-        repoPath := init_dir.ReadFromReg(os.Args[3])
-        vcs := init_dir.NewVCS(repoPath)
+        if currentRepoPath == "" {
+            pterm.Error.Println("You must start a session before using this command.")
+            return
+        }
+        vcs := init_dir.NewVCS(currentRepoPath)
         err := vcs.Rollback()
         if err != nil {
             pterm.Error.Println("Error pullback:", err)
@@ -85,8 +123,8 @@ func check_args(){
         pterm.Error.Printfln("Unknown command: %s\n", os.Args[1])
         printHelp()
     }
-
 }
+
 func main() {
-    check_args()
+    checkArgs()
 }
